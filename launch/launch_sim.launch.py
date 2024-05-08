e39eba96081c5092fb245d5a008baaf7ed4c80d7
ALAN <alanjcyriac@gmail.com>
2024-05-08 11:29:52 +0530

    rsp.launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,12 +22,11 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo_params_path = os.path.join(
-                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_path = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
